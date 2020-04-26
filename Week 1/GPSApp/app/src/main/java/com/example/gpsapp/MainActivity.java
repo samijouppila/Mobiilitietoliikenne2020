f47319c43b08331bf4469b85a8856eb82b266304
Sami Jouppila <sami.jouppila@gmail.com>
2020-04-26 13:55:54 +0300

    GPSApp added. Lab 1 Assignments 1 & 2

---
+++ Week 1/GPSApp/app/src/main/java/com/example/gpsapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.gpsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    TextView gpsField;
+    TextView locationField;
+    int locationRequestCode;
+    Geocoder geocoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        gpsField = findViewById(R.id.gpsField);
+        locationField = findViewById(R.id.locationField);
+        geocoder = new Geocoder(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, locationRequestCode);
+        }
+        updateLocation();
+    }
+
+    private void updateLocation() {
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location!= null) {
+                    gpsField.setText(location.getLatitude() + ", " + location.getLongitude());
+                    try {
+                        List<Address> list = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                        Address address = list.get(0);
+                        locationField.setText(address.getLocality());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+
+}
